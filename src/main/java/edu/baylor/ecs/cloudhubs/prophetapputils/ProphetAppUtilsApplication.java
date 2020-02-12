91e8b40bc661a20da63dffa4beb555a16b36bfa5
Jan Svacina <jan@svacina.net>
2020-02-12 15:32:09 -0600

    init commit

---
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetapputils/ProphetAppUtilsApplication.java
@@ -0,0 +1,13 @@
+package edu.baylor.ecs.cloudhubs.prophetapputils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProphetAppUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProphetAppUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/baylor/ecs/cloudhubs/prophetapputils/ProphetAppUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.baylor.ecs.cloudhubs.prophetapputils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProphetAppUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
