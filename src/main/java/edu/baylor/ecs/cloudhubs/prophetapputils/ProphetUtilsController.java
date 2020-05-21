7b4e929481bed81a6637865628209df073b3cb90
Jan Svacina <jan@svacina.net>
2020-05-21 16:14:07 -0500

    cross origin on api

--- src/main/java/edu/baylor/ecs/cloudhubs/prophetapputils/ProphetUtilsController.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophetapputils/ProphetUtilsController.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,7 @@ public class ProphetUtilsController {
     Logger logger = LoggerFactory.getLogger(ProphetUtilsController.class);
 
     @PostMapping("/")
+    @CrossOrigin("*")
     public ResponseEntity<ProphetAppData> getMultiRepoAppData(@RequestBody GitReq request) {
         String dirName = "repos-" + LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond();
         File dir = new File(dirName);
